10d2fce76e8f271921cb0478bc9d04fcd6421fb3
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-01-06 20:27:55 +0100

    fixed MontanaBlack npc model and texture

--- src/main/java/com/montanablackmod/client/renderer/MontanaBlackRenderer.java
+++ src/main/java/com/montanablackmod/client/renderer/MontanaBlackRenderer.java
@@ -2,19 +2,15 @@
 package com.montanablackmod.client.renderer;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
-import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.model.HumanoidModel;
 
 import com.montanablackmod.entity.MontanaBlackEntity;
+import com.montanablackmod.client.model.Modelcustom_model;
 
-public class MontanaBlackRenderer extends HumanoidMobRenderer<MontanaBlackEntity, HumanoidModel<MontanaBlackEntity>> {
+public class MontanaBlackRenderer extends MobRenderer<MontanaBlackEntity, Modelcustom_model<MontanaBlackEntity>> {
 	public MontanaBlackRenderer(EntityRendererProvider.Context context) {
-		super(context, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER)), 0.5f);
-		this.addLayer(new HumanoidArmorLayer(this, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)),
-				new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR))));
+		super(context, new Modelcustom_model(context.bakeLayer(Modelcustom_model.LAYER_LOCATION)), 0.5f);
 	}
 
 	@Override
